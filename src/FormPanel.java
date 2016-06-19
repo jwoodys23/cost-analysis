fe1460c245fb0ff567d88c5536f1ca180df1e2ef
Jourdan Woodrich <jourdanwoodrich@gmail.com>
2016-06-19 17:53:36 -0400

    Begin adding labels and textfield

--- src/FormPanel.java
+++ src/FormPanel.java
@@ -6,10 +6,25 @@ import java.awt.*;
  * Created by jourdanwoodrich on 6/19/16.
  */
 public class FormPanel extends JPanel {
+
+    private JLabel partNameLabel;
+    private JLabel partNumberLabel;
+    private JTextField partNameField;
+    private JTextField partNumberField;
+    private JButton okBtn;
+
+
     public FormPanel() {
         Dimension dim = getPreferredSize();
         dim.width = 250;
         setPreferredSize(dim);
+
+        partNameLabel = new JLabel("Part name: ");
+        partNumberLabel = new JLabel("Part Number: ");
+        partNameField = new JTextField(20);
+        partNumberField = new JTextField(20);
+        okBtn = new JButton("Save Part");
+
 
         Border innerBorder = BorderFactory.createTitledBorder("Add New Part");
         Border outerBorder = BorderFactory.createEmptyBorder(5,5,5,5);
