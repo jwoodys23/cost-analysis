cb9f5c03ce6a879236e505f32651089ecb2be8f2
Jourdan Woodrich <jourdanwoodrich@gmail.com>
2016-06-20 20:47:11 -0400

    Add FormEvent class

---
+++ src/FormEvent.java
@@ -0,0 +1,10 @@
+import java.util.EventObject;
+
+/**
+ * Created by jourdanwoodrich on 6/20/16.
+ */
+public class FormEvent extends EventObject{
+    public FormEvent(Object source){
+        super(source);
+    }
+}
