82dea402b21730e9a4adc5dbddbcd7efdae0332b
Jourdan Woodrich <jourdanwoodrich@gmail.com>
2016-06-26 11:47:21 -0400

    Worked on layout for results panel and form

--- src/gui/ResultForm.java
+++ src/gui/ResultForm.java
@@ -1,7 +1,43 @@
 package gui;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by jourdanwoodrich on 6/25/16.
  */
-public class ResultForm {
+public class ResultForm extends JPanel{
+
+    private JLabel stdLaborRate;
+    private JButton saveBtn;
+
+    public ResultForm(){
+
+        Dimension dim = getPreferredSize();
+        dim.width = 275;
+        setPreferredSize(dim);
+
+        stdLaborRate = new JLabel("Standard Labor Rate");
+        saveBtn = new JButton("Save Changes");
+
+        setLayout(new GridBagLayout());
+
+        GridBagConstraints gc = new GridBagConstraints();
+
+//        gc.gridx = 0;
+//        gc.gridy = 0;
+
+        gc.weightx = 1;
+        gc.weighty = 2.0;
+        gc.gridx = 1;
+        gc.gridy = 6;
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        gc.insets = new Insets(0,0,0,0);
+        add(saveBtn, gc);
+
+
+
+
+
+    }
 }
---
+++ src/gui/VariancePanel.java
@@ -0,0 +1,116 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by jourdanwoodrich on 6/26/16.
+ */
+public class VariancePanel extends JPanel {
+    //Titles
+    private JLabel materialLabel;
+    private JLabel laborLabel;
+    private JLabel overheadVariance;
+    private JLabel totalVariance;
+    private JLabel stdCost;
+    private JLabel actualCost;
+    private JLabel variance;
+    private JLabel grossMargin;
+
+    //Labels with variable passed to the constructor
+    private JLabel materialStd;
+    private JLabel laborStd;
+    private JLabel overheadStd;
+    private JLabel totalStd;
+    private JLabel materialActual;
+    private JLabel laborActual;
+    private JLabel overheadActual;
+    private JLabel totalActual;
+    private JLabel materialVariance;
+    private JLabel laborVariance;
+
+    public VariancePanel(){
+        //JPanel = new JPanel();
+        JPanel graphPanel = new JPanel();
+
+        //String filler = "filler";
+
+        stdCost = new JLabel("Standard Cost");
+        actualCost = new JLabel("Actual Cost");
+        variance = new JLabel("Variance");
+        materialLabel = new JLabel("Material Variance");
+        materialStd = new JLabel("Std material cost");
+        materialActual = new JLabel("Actual material cost");
+        materialVariance = new JLabel("total material var");
+        laborLabel = new JLabel("Labor Variance");
+        laborActual = new JLabel("Actual Labor cost");
+        laborStd = new JLabel("Std Labor Cost");
+        laborVariance = new JLabel("total labor var");
+
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints gc = new GridBagConstraints();
+
+        gc.weightx = .2;
+        gc.weighty = 0.2;
+
+        //////// FIRST ROW /////////////
+        gc.gridx = 1;
+        gc.gridy = 0;
+        //gc.fill = GridBagConstraints.NONE;
+        //gc.anchor = GridBagConstraints.LINE_END;
+        //gc.insets = new Insets(0,0,0,5);
+        add(stdCost, gc);
+
+        gc.gridx = 2;
+        gc.gridy = 0;
+        //gc.anchor = GridBagConstraints.LINE_START;
+        //gc.insets = new Insets(0,0,0,0);
+        add(actualCost, gc);
+
+        gc.gridx = 3;
+        add(variance,gc);
+
+        //////////////Next Row///////////////
+
+        gc.weightx = .2;
+        gc.weighty = 0.2;
+
+        gc.gridy = 1;
+        gc.gridx = 0;
+        add(materialLabel, gc);
+
+        gc.gridx = 1;
+        add(materialStd,gc);
+
+        gc.gridx = 2;
+        add(materialActual, gc);
+
+        gc.gridx = 3;
+        add(materialVariance, gc);
+
+        //////////////Next Row///////////////
+
+        gc.weightx = 1;
+        gc.weighty = 0.2;
+
+        gc.gridy = 2;
+        gc.gridx = 0;
+        add(laborLabel, gc);
+
+        gc.gridx = 1;
+        add(laborStd,gc);
+
+        gc.gridx = 2;
+        add(laborActual, gc);
+
+        gc.gridx = 3;
+        add(laborVariance, gc);
+
+        /// add Subpanels to dialog////
+        //setLayout(new BorderLayout());
+        //add( BorderLayout.CENTER);
+        //add(graphPanel, BorderLayout.SOUTH);
+
+    }
+}
